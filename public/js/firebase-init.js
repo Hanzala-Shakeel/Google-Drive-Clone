46a31debbd6d1692659f671e9fc0a178e18d08c5
Hanzala-Shakeel <102066085+Hanzala-Shakeel@users.noreply.github.com>
2024-01-07 23:25:27 +0500

    Update firebase-init.js

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -1,13 +1,12 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAMIGE05GZIbwtIM-o2vqau1OJ2TzoTijo",
-  authDomain: "my-portfolio-9b79a.firebaseapp.com",
-  projectId: "my-portfolio-9b79a",
-  storageBucket: "my-portfolio-9b79a.appspot.com",
-  messagingSenderId: "871850101937",
-  appId: "1:871850101937:web:60892850ed6d27254c6690",
-  measurementId: "G-ZQQHYSCVLK"
+  apiKey: "AIzaSyDzJ1DbKXCOe8rZJSB7y2XflS6GIbQAN_E",
+  authDomain: "my-drive-5eabd-4112f.firebaseapp.com",
+  projectId: "my-drive-5eabd",
+  storageBucket: "my-drive-5eabd.appspot.com",
+  messagingSenderId: "415937145166",
+  appId: "1:415937145166:web:7415eb641d410bc954961f"
 };
 
 // Initialize Firebase
